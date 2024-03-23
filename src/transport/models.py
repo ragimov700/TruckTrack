f84bcf65eb8fa4ee2f326ff511720e155c4fdf4a
Sherif Ragimov <ragimov700@yandex.ru>
2024-03-23 22:22:16 +0400

    models for transport app

--- src/transport/models.py
+++ src/transport/models.py
@@ -1,13 +1,108 @@
+from django.core.validators import RegexValidator, MinValueValidator, \
+    MaxValueValidator
 from django.db import models
-
-
-class Vehicle(models.Model):
-    pass
+from src.transport.constants import NUMBER_REGEX, MIN_CAPACITY, MAX_CAPACITY, \
+    MIN_CARGO_WEIGHT, MAX_CARGO_WEIGHT
 
 
 class Location(models.Model):
-    pass
+    """
+    Модель локации. Содержит информацию о городе, штате и почтовом индексе,
+    а также широте и долготе.
+    """
+    city = models.CharField(max_length=100, verbose_name='город')
+    state = models.CharField(max_length=2, verbose_name='штат')
+    zip_code = models.CharField(max_length=5, verbose_name='почтовый индекс')
+    latitude = models.DecimalField(
+        max_length=8,
+        decimal_places=5,
+        verbose_name='широта'
+    )
+    longitude = models.DecimalField(
+        max_length=8,
+        decimal_places=5,
+        verbose_name='долгота'
+    )
+
+    def __str__(self):
+        return f'{self.city}, {self.state} {self.zip_code}'
+
+    class Meta:
+        verbose_name = 'локация'
+        verbose_name_plural = 'локации'
 
 
 class Cargo(models.Model):
-    pass
+    """
+    Модель груза. Содержит информацию о местах погрузки и разгрузки,
+    весе и описании груза.
+    """
+    pickup_location = models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE,
+        related_name='pickups',
+        verbose_name='место погрузки'
+    )
+    delivery_location = models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE,
+        related_name='deliveries',
+        verbose_name='место разгрузки'
+    )
+    weight = models.PositiveIntegerField(
+        validators=(
+            MinValueValidator(MIN_CARGO_WEIGHT),
+            MaxValueValidator(MAX_CARGO_WEIGHT)
+        ),
+        verbose_name='вес'
+    )
+    description = models.TextField(
+        max_length=1000,
+        verbose_name='описание груза'
+    )
+
+    def __str__(self):
+        return f'Груз из {self.pickup_location} в {self.delivery_location}'
+
+    class Meta:
+        verbose_name = 'груз'
+        verbose_name_plural = 'грузы'
+
+
+class Truck(models.Model):
+    """
+    Модель грузовика. Содержит информацию о номерном знаке, грузоподъемности
+    и текущем местоположении.
+    """
+    plate_number = models.CharField(
+        max_length=6,
+        verbose_name='номерной знак',
+        unique=True,
+        validators=(
+            RegexValidator(
+                regex=NUMBER_REGEX,
+                message='Номерной знак должен иметь формат: "1234A"'
+            ),
+        )
+    )
+    capacity = models.PositiveIntegerField(
+        verbose_name='грузоподъемность',
+        validators=(
+            MinValueValidator(MIN_CAPACITY),
+            MaxValueValidator(MAX_CAPACITY)
+        )
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='местоположение'
+    )
+
+    def __str__(self):
+        return f'Грузовик {self.plate_number}'
+
+    class Meta:
+        verbose_name = 'грузовик'
+        verbose_name_plural = 'грузовики'
+
