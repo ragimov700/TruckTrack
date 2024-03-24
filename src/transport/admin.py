bb8e1a27e42440534fbd4401e907f7984ea98740
Sherif Ragimov <ragimov700@yandex.ru>
2024-03-24 11:58:33 +0400

    admin config

--- src/transport/admin.py
+++ src/transport/admin.py
@@ -3,16 +3,29 @@ from django.contrib import admin
 from transport.models import Cargo, Location, Truck
 
 
-@admin.register(Truck)
-class TruckAdmin(admin.ModelAdmin):
-    autocomplete_fields = ['location']
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    """
+    Конфигурация админ-панели для модели груза.
+    """
+    list_display = ('id', 'pickup_location', 'delivery_location', 'weight')
+    autocomplete_fields = ['pickup_location', 'delivery_location']
 
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    search_fields = ['city', 'state', 'zip']
+    """
+    Конфигурация админ-панели для модели локации.
+    """
+    list_display = ('id', 'city', 'state', 'zip_code')
+    search_fields = ('city', 'state', 'zip_code')
 
 
-@admin.register(Cargo)
-class CargoAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Truck)
+class TruckAdmin(admin.ModelAdmin):
+    """
+    Конфигурация админ-панели для модели грузовика.
+    """
+    list_display = ('id', 'plate_number', 'capacity', 'location',)
+    search_fields = ('plate_number',)
+    autocomplete_fields = ('location',)
